1a27412627ad48df91b6ad7f16faa47af9c9df36
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-02-24 23:08:47 -0600

    created feature-feeling but its not sending the feedback to the reducer correctly

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,7 +1,43 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+
 function Feeling() {
-    return(
+
+    const dispatch = useDispatch();
+    
+    let [feeling, setFeeling] = useState({ feeling: 0});
+
+    const handleFeelingChange = (event) => {
+        setFeeling({
+            feeling: feeling,
+        });
+    }
+
+    const addFeeling = (event) => {
+        event.preventDefault();
+
+        dispatch({
+            type: "ADD_FEEDBACK",
+            payload: feeling
+        })
+
+        setFeeling('');
+    };
+
+    return (
         <>
-            <p>Feeling</p>
+            <h2>How are you feeling today?</h2>
+
+            <form onSubmit={(event) => addFeeling(event)}>
+                <input
+                    onChange={handleFeelingChange}
+                    type='number'
+                    placeholder='feeling?'
+                />
+
+                <button type='submit'>Next</button>
+            </form>
         </>
     )
 }
